e943059491407ac0009ee4d547ac475e241b6252
leikezej <jezedevkiel21@gmail.com>
2022-12-11 05:41:47 +0800

    Push Latest Updates

---
+++ src/pages/register/Register copy.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./register.scss";
+import axios from "axios";
+
+const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    password: "",
+    website: "",
+    cover_image: "",
+    profile_image: ""
+  });
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/signup", inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  console.log(err)
+
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Lama Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="phone"
+              placeholder="Phone"
+              name="phone"
+              onChange={handleChange}
+            />
+            <input
+              type="address"
+              placeholder="Address"
+              name="address"
+              onChange={handleChange}
+            />
+            <input
+              type="website"
+              placeholder="Website"
+              name="website"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="file"
+              accept="image/*"
+              placeholder="Cover_image"
+              name="cover_image"
+              onChange={handleChange}
+            />
+            <input
+              accept="image/*"
+              type="file"
+              placeholder="Profile_image"
+              name="profile_image"
+              onChange={handleChange}
+            />
+
+            {err && err}
+            <button onClick={handleClick}>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,120 +1,159 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+// import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+import "./register.css";
 import "./register.scss";
-import axios from "axios";
 
-const Register = () => {
-  const [inputs, setInputs] = useState({
-    username: "",
-    name: "",
-    email: "",
-    phone: "",
-    address: "",
-    password: "",
-    website: "",
-    cover_image: "",
-    profile_image: ""
-  });
-  const [err, setErr] = useState(null);
+import { AiOutlineMail } from "react-icons/ai";
+import { FiEye, FiEyeOff, FiKey, FiPhone, FiUser, FiFacebook, FiGithub, FiLinkedin  } from "react-icons/fi";
 
-  const handleChange = (e) => {
-    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+function Register() {
+   const navigate = useNavigate('');
+   const [ name, setName ] = useState('');
+   const [ email, setEmail ] = useState('');
+   const [ image, setImage ] = useState('');
+   const [ phone, setPhone ] = useState('');
+   const [ password, setPassword ] = useState('');
+   
+   const [ showPassword, setShowPassword ] = useState(false);
+   const changeIcon = showPassword === true ? false : true;
+      
+   const togglePassword = () => {
+    setShowPassword(!showPassword);
+   };
+   
+//       useEffect(() => {
+//          if(!localStorage.getItem('accessToken')) {
+//         navigate('/')
+//     }
+//   }, [])
+   
+   const handleRegister = () => {
+      console.log({ name, email, phone, password })
+      axios.post('http://localhost:272/api/auth/signup', 
+      {
+         name: name,
+         email: email,
+         image: image,
+         phone: phone,
+         password: password
+      })
+         .then(res => {
+         // if (!name || !email || !phone || !image ||  !password) {
+         //        return res.sendStatus(400);
+         //     }
+            console.log(res.data)
+            console.log(res.status)
+               alert('Registration Successfull!')
+            navigate("/", { replace: true });
+         })
+         .catch(error => {
+            console.log(error)
+            alert('Register Error')
+         })
+   }
+   
+   return (<>
+         <h1 className="center" style={{ fontFamily: 'Kaushan Script', marginTop: '50px'}}> REGISTER </h1>
+               <Link to={'/'} style={{  textAlign: 'center', color: '#000', display: 'block' }}> Login Here</Link>
+         <br />
+                     
+         <div className="outcard">
+           Full Name: <br /> 
+          <FiUser /> {" "}
+               <input
+               onChange={(e) => {
+                  setName(e.target.value)
+               }}
+               required={true}
+               placeholder="Johnny Pusong"
+               value={name}
+               className="password-inputs"
+               type="name" /> <br /> <br />
+            
+            Email: <br />
+             <AiOutlineMail />  {" "}
+             <input
+               required={true}
+               onChange={(e) => {
+                  setEmail(e.target.value)
+               }}
+               placeholder="johndoe@gmail.com"
+               value={email}
+               className="password-inputs"
+               type="email" /> <br /> <br />
+            
+            Phone: <br />
+          <FiPhone />{" "}
+             <input
+               required={true}
+               onChange={(e) => {
+                  setPhone(e.target.value)
+               }}
+               value={phone}
+               className="password-inputs"
+               type="number"
+                placeholder="(xxx) xxx-xxxx"
+               /> <br /> <br />
+               
+                           Avatar: <br />
+          <FiPhone />{" "}
+             <input
+               required={true}
+               onChange={(e) => {
+                  setImage(e.target.value)
+               }}
+               value={image}
+               className="password-inputs"
+               type="file"
+               /> <br /> <br />
+               
+            
+            Password: <br />
+             <FiKey />{" "}
+               <input 
+                  onChange={(e) => {
+                     setPassword(e.target.value)
+                  }}
+                  placeholder={'************'}
+                  value={password}
+               required={true}
+                  type={showPassword ? "text" : "password"}
+                  className="password-inputs" /> {' '}
+               <span
+                    onClick={() => {
+                       togglePassword(changeIcon);
+                    }}
+                 >
+                    {changeIcon ? <FiEye /> : <FiEyeOff />}
+                 </span>
+                  <br /> <br />
+               <button onClick={handleRegister} className="btns"> Register </button>
+            <>
+            <br />
+               <center style={{ textAlign: 'center', justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>Or Register With</center> <br />
+               <center>
+                     <a href="https://www.linkedin.com/in/jezekiel-isip-1ab872215/" >
+                        {" "}<FiLinkedin size="30px" color="#000" />{" "}
+                    </a>
+                    
+                    <a href="https://github.com/leikezej">
+                        {" "}<FiGithub size="30px" color="#000" 
+                        />{" "}
+                    </a>
+                    
+                    <a href="https://www.facebook.com/thebullier">
+                       {" "} <FiFacebook  size="30px" color="#000"
+                        />{" "}
+                    </a>
+               </center>
+            </>
+         </div>
+      </>
+   )
+}
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-
-    try {
-      await axios.post("http://localhost:8800/api/auth/signup", inputs);
-    } catch (err) {
-      setErr(err.response.data);
-    }
-  };
-
-  console.log(err)
-
-  return (
-    <div className="register">
-      <div className="card">
-        <div className="left">
-          <h1>Lama Social.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
-          <span>Do you have an account?</span>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-        </div>
-        <div className="right">
-          <h1>Register</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              onChange={handleChange}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-            />
-            <input
-              type="phone"
-              placeholder="Phone"
-              name="phone"
-              onChange={handleChange}
-            />
-            <input
-              type="address"
-              placeholder="Address"
-              name="address"
-              onChange={handleChange}
-            />
-            <input
-              type="website"
-              placeholder="Website"
-              name="website"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-            />
-            <input
-              type="file"
-              accept="image/*"
-              placeholder="Cover_image"
-              name="cover_image"
-              onChange={handleChange}
-            />
-            <input
-              accept="image/*"
-              type="file"
-              placeholder="Profile_image"
-              name="profile_image"
-              onChange={handleChange}
-            />
-
-            {err && err}
-            <button onClick={handleClick}>Register</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default Register;
